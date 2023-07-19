0c86ae4a48a91ab03e5b8d1ad5daeb7c292bf0e0
Jesus Lopez <jesuslv2412@hotmail.com>
2023-07-18 19:14:16 -0600

    err 404

---
+++ 404.php
@@ -0,0 +1,38 @@
+<?php /* Template Name: Datos */ ?>
+
+<?php get_header(); ?>
+
+<!--Main layout-->
+    <main>
+        <div class="container">
+
+            <!--Section: Nosotros-->
+            <section class="mt-5">
+                <div class="row">
+                    <div class="col-md-12">
+                        <br/><br/><br/><br/><br/>
+                    </div>
+                    <div class="col-md-12 text-center">
+                        <h1 class="tituse1">Página no encontrada</h1>
+
+                        <div class="err-404">
+                                <p>404</p>
+                        </div>
+
+                    </div>
+                    <div class="col-md-12 text-center mb-5">
+                        <h3><a href="<?php bloginfo('url'); ?>">Ir a inicio</a></h3>
+                    </div>
+                </div>
+            </section>
+            <!--Section: Nosotros-->
+        </div>
+
+
+
+    </main>
+    <!--Main layout-->
+
+
+
+<?php get_footer(); ?>
