a8a35d6f005f8eaee8189fb67db95b62cd62cd64
Jesus Lopez <jesuslv2412@hotmail.com>
2023-07-18 19:03:12 -0600

    author page

--- page-autores.php
+++ page-autores.php
@@ -17,39 +17,6 @@ Template Name: Autores
         <div class="col-md-12"><br/></div>
       </div>
 
-      <!--<div class="row">
-        <div class="col-md-12">
-          <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item"><a class="page-link" href="#">A</a></li>
-              <li class="page-item"><a class="page-link" href="#">B</a></li>
-              <li class="page-item"><a class="page-link" href="#">C</a></li>
-              <li class="page-item"><a class="page-link" href="#">D</a></li>
-              <li class="page-item"><a class="page-link" href="#">E</a></li>
-              <li class="page-item"><a class="page-link" href="#">F</a></li>
-              <li class="page-item"><a class="page-link" href="#">G</a></li>
-              <li class="page-item"><a class="page-link" href="#">H</a></li>
-              <li class="page-item"><a class="page-link" href="#">I</a></li>
-              <li class="page-item"><a class="page-link" href="#">J</a></li>
-              <li class="page-item"><a class="page-link" href="#">K</a></li>
-              <li class="page-item"><a class="page-link" href="#">L</a></li>
-              <li class="page-item"><a class="page-link" href="#">M</a></li>
-              <li class="page-item"><a class="page-link" href="#">N</a></li>
-              <li class="page-item"><a class="page-link" href="#">O</a></li>
-              <li class="page-item"><a class="page-link" href="#">P</a></li>
-              <li class="page-item"><a class="page-link" href="#">Q</a></li>
-              <li class="page-item"><a class="page-link" href="#">R</a></li>
-              <li class="page-item"><a class="page-link" href="#">S</a></li>
-              <li class="page-item"><a class="page-link" href="#">T</a></li>
-              <li class="page-item"><a class="page-link" href="#">U</a></li>
-              <li class="page-item"><a class="page-link" href="#">V</a></li>
-              <li class="page-item"><a class="page-link" href="#">W</a></li>
-              <li class="page-item"><a class="page-link" href="#">X</a></li>
-              <li class="page-item"><a class="page-link" href="#">Y</a></li>
-              <li class="page-item"><a class="page-link" href="#">Z</a></li>
-            </ul>
-          </nav>
-        </div>-->
       </div>
     </section>
   </div>
@@ -59,7 +26,7 @@ Template Name: Autores
   <div class="row">
     <?php foreach (get_terms('Autores') as $cat) : ?>
 
-      <div class="col-md-6">
+      <div class="col-md-6 card-author">
           <div class="row">
             <div class="col-md-4 images-autor"><!-- IMG AUTOR -->
               <img src="<?php echo z_taxonomy_image_url($cat->term_id); ?>" />
